35e450bca311c1224f7384e4d6a2ea3da2acae96
Quentin <quentin.chevy@gmail.com>
2021-06-27 21:16:37 -0400

    trial related to morecleaning commit of Bpod Function

--- CuedOutcome/CuedOutcome.m
+++ CuedOutcome/CuedOutcome.m
@@ -72,7 +72,7 @@ if S.GUI.Optogenetic
     load('C:\Users\Kepecs\Documents\Data\Quentin\Bpod\Protocols\PhotoStim_multiFreq\LightTrain_6s.mat');
     ProgramPulsePal(ParameterMatrix);
 end
-%% NIDAQ Initialization amd Plots
+%% NIDAQ Initialization and Plots
 if S.GUI.Photometry || S.GUI.Wheel
     if (S.GUI.DbleFibers+S.GUI.Isobestic405+S.GUI.RedChannel)*S.GUI.Photometry >1
         disp('Error - Incorrect photometry recording parameters')
@@ -80,15 +80,7 @@ if S.GUI.Photometry || S.GUI.Wheel
     end
     Nidaq_photometry('ini',ParamPC);
 end
-if S.GUI.Photometry
-    FigNidaq1=Online_PhotoPlot('ini','470');
-    if S.GUI.DbleFibers || S.GUI.Isobestic405 || S.GUI.RedChannel
-        FigNidaq2=Online_PhotoPlot('ini','channel2');
-    end
-end
-if S.GUI.Wheel
-    FigWheel=Online_WheelPlot('ini');
-end
+[FigPhoto1,FigPhoto2,FigWheel]=Nidaq_Plots('ini');
 
 %% Bonsai
 if S.GUI.Bonsai
@@ -220,28 +212,7 @@ end
 try
 [currentOutcome, currentLickEvents]=Online_LickEvents(S.Names.StateToZero{S.GUI.StateToZero});
 FigLick=Online_LickPlot('update',[],FigLick,currentOutcome,currentLickEvents);
-
-if S.GUI.Photometry
-    [currentNidaq1, rawNidaq1]=Photometry_demod(PhotoData(:,1),nidaq.LED1,S.GUI.LED1_Freq,S.GUI.LED1_Amp,S.Names.StateToZero{S.GUI.StateToZero});
-    currentNidaq1=Online_VariableITIAVG(currentNidaq1,'PreState');
-    FigNidaq1=Online_PhotoPlot('update',[],FigNidaq1,currentNidaq1,rawNidaq1);
-
-    if S.GUI.Isobestic405 || S.GUI.DbleFibers || S.GUI.RedChannel
-        if S.GUI.Isobestic405
-        [currentNidaq2, rawNidaq2]=Photometry_demod(PhotoData(:,1),nidaq.LED2,S.GUI.LED2_Freq,S.GUI.LED2_Amp,S.Names.StateToZero{S.GUI.StateToZero});
-        elseif S.GUI.RedChannel
-        [currentNidaq2, rawNidaq2]=Photometry_demod(Photo2Data(:,1),nidaq.LED2,S.GUI.LED2_Freq,S.GUI.LED2_Amp,S.Names.StateToZero{S.GUI.StateToZero});
-        elseif S.GUI.DbleFibers
-        [currentNidaq2, rawNidaq2]=Photometry_demod(Photo2Data(:,1),nidaq.LED2,S.GUI.LED1b_Freq,S.GUI.LED1b_Amp,S.Names.StateToZero{S.GUI.StateToZero});
-        end
-        currentNidaq2=Online_VariableITIAVG(currentNidaq2,'PreState');
-        FigNidaq2=Online_PhotoPlot('update',[],FigNidaq2,currentNidaq2,rawNidaq2);
-    end
-end
-
-if S.GUI.Wheel
-    FigWheel=Online_WheelPlot('update',FigWheel,WheelData,S.Names.StateToZero{S.GUI.StateToZero},currentTrial,currentLickEvents);
-end
+[FigPhoto1,FigPhoto2,FigWheel]=Nidaq_Plots('update',FigPhoto1,FigPhoto2,FigWheel,'PreState');
 catch
     disp('Oups, something went wrong with the online analysis... May be you closed a plot ?') 
 end
