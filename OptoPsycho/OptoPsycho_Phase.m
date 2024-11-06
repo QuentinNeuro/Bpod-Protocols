d8e9947d31997797f320713bd075879fc6cae1d2
Quentin <quentin.chevy@gmail.com>
2024-11-06 12:03:06 -0500

    typo

--- OptoPsycho/OptoPsycho_Phase.m
+++ OptoPsycho/OptoPsycho_Phase.m
@@ -1,6 +1,6 @@
 function [trialsNames, trialsMatrix]=OptoPsycho_Phase(S)
 
-OptoPowers=linspace(S.GUI.PowerMin,S.GUI.PowerMax,S.GUI.Opto_PowerNb);
+OptoPowers=linspace(S.GUI.Opto_PowerMin,S.GUI.Opto_PowerMax,S.GUI.Opto_PowerNb);
 for p=1:S.GUI.Opto_PowerNb
     trialsNames{p}=sprintf('Power_%.2d',OptoPowers(p));
     trialsMatrix(p,:)=[p, 1/S.GUI.Opto_PowerNb, OptoPowers(p), S.GUI.Delay, S.GUI.RewardValve, S.InterRew, double('o')];
