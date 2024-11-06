be93f1ce1eb70ce5f0175b172466f4b5f5787762
Quentin <quentin.chevy@gmail.com>
2024-11-06 10:32:20 -0500

    OptoPsychometric_ini

---
+++ OptoLickToReward/OptoLickToReward.m
@@ -0,0 +1,183 @@
+function OptoLickToReward
+%Functions used in this protocol:
+%"CuedReward_Phase": specify the phase of the training
+%"WeightedRandomTrials" : generate random trials sequence
+
+%"Online_LickPlot"      : initialize and update online lick and outcome plot
+%"Online_LickEvents"    : extract the data for the online lick plot
+%"Online_NidaqPlot"     : initialize and update online nidaq plot
+%"Online_NidaqEvents"   : extract the data for the online nidaq plot
+
+global BpodSystem nidaq S
+
+%% Define parameters
+S = BpodSystem.ProtocolSettings; % Load settings chosen in launch manager into current workspace as a struct called S
+ParamPC=BpodParam_PCdep();
+if isempty(fieldnames(S))  % If settings file was an empty struct, populate struct with default settings
+    OptoLickToReward_TaskParameters(ParamPC);
+end
+
+% Initialize parameter GUI plugin and Pause
+BpodParameterGUI('init', S);
+BpodSystem.Pause=1;
+HandlePauseCondition;
+S = BpodParameterGUI('sync', S);
+
+S.SmallRew  =   GetValveTimes(S.GUI.SmallReward, S.GUI.RewardValve);
+S.InterRew  =   GetValveTimes(S.GUI.InterReward, S.GUI.RewardValve);
+S.LargeRew  =   GetValveTimes(S.GUI.LargeReward, S.GUI.RewardValve);
+
+%% Trial Sequence
+[S.TrialsNames, S.TrialsMatrix]=OptoLickToReward_Phase(S);
+TrialSequence=WeightedRandomTrials(S.TrialsMatrix(:,2)', S.GUI.MaxTrials);
+S.NumTrialTypes=max(TrialSequence);
+FigLick=Online_LickPlot('ini',TrialSequence);
+
+%% Stimulation
+if S.GUI.Optogenetic
+    PulsePal(ParamPC.PPCOM);
+    S.PulsePalProtocol=S.Names.PPProtocols{S.GUI.PulsePalProtocol};
+    load(S.PulsePalProtocol);
+    ParameterMatrix{5,2}=S.GUI.CueDuration;
+    ParameterMatrix{8,2}=S.GUI.CueDuration;
+    S.ParameterMatrix=ParameterMatrix;
+    S.OptoPowers=linspace(S.GUI.PowerMin,S.GUI.PowerMax,S.GUI.Opto_PowerNb);
+end
+%% NIDAQ Initialization and Plots
+if S.GUI.Photometry || S.GUI.Wheel
+    if (S.GUI.DbleFibers+S.GUI.Isobestic405+S.GUI.RedChannel)*S.GUI.Photometry >1
+        disp('Error - Incorrect photometry recording parameters')
+        return
+    end
+    Nidaq_photometry('ini',ParamPC);
+end
+[FigPhoto1,FigPhoto2,FigWheel]=Online_NidaqPlots('ini');
+
+%% Bonsai
+if S.GUI.Bonsai
+    BpodSystem.Pause=1;
+    disp('Adjust ROI - resume when ready');
+    success=Bpod2Bonsai_Quentin()
+    HandlePauseCondition;
+end
+
+%% Main trial loop
+BpodSystem.Data.TrialTypes = []; % The trial type of each trial completed will be added here.
+for currentTrial = 1:S.GUI.MaxTrials
+    S = BpodParameterGUI('sync', S); % Sync parameters with BpodParameterGUI plugin 
+%% Initialize current trial parameters
+    S.ITI = 100;
+    while S.ITI > 3 * S.GUI.ITI
+        S.ITI = exprnd(S.GUI.ITI);
+    end
+%% Opto
+    ParameterMatrix{3,2}=trialsMatrix(TrialSequence(currentTrial),3);
+    S.ParameterMatrix=ParameterMatrix;
+    ProgramPulsePal(ParameterMatrix);
+
+%% Assemble State matrix
+ 	sma = NewStateMatrix();
+    sma = AddState(sma,'Name', 'ITI',...
+        'Timer',S.ITI,...
+        'StateChangeConditions', {'Tup', 'PreState'},...
+        'OutputActions',{});
+    %Pre task states
+    sma = AddState(sma, 'Name','PreState',...
+        'Timer',S.GUI.PreCue,...
+        'StateChangeConditions',{'Tup','CueDelivery'},...
+        'OutputActions',{'BNCState',1});
+    %Stimulus delivery
+    sma=AddState(sma,'Name', 'CueDelivery',...
+        'Timer',S.GUI.CueDuration,...
+        'StateChangeConditions',{'Tup', 'PostOutcome','Port1In','Delay'},...
+        'OutputActions', {'BNCState',2});
+    %Delay
+    sma=AddState(sma,'Name', 'Delay',...
+        'Timer',S.Delay,...
+        'StateChangeConditions', {'Tup', 'Outcome'},...
+        'OutputActions', {});
+    %Reward
+    sma=AddState(sma,'Name', 'Outcome',...
+        'Timer',S.Outcome,...
+        'StateChangeConditions', {'Tup', 'PostOutcome'},...
+        'OutputActions', {'ValveState', S.Valve});  
+    %Post task states
+    sma=AddState(sma,'Name', 'PostOutcome',...
+        'Timer',S.GUI.PostOutcome,...
+        'StateChangeConditions',{'Tup', 'End'},...
+        'OutputActions',{});
+    sma = AddState(sma,'Name', 'End',...
+        'Timer',1,...
+        'StateChangeConditions', {'Tup', 'exit'},...
+        'OutputActions',{});
+    SendStateMatrix(sma);
+ 
+%% NIDAQ Get nidaq ready to start
+if S.GUI.Photometry || S.GUI.Wheel
+    Nidaq_photometry('WaitToStart');
+end
+     RawEvents = RunStateMatrix;
+    
+%% NIDAQ Stop acquisition and save data in bpod structure
+if S.GUI.Photometry || S.GUI.Wheel
+    Nidaq_photometry('Stop');
+    [PhotoData,WheelData,Photo2Data]=Nidaq_photometry('Save');
+    if S.GUI.Photometry
+        BpodSystem.Data.NidaqData{currentTrial}=PhotoData;
+        if S.GUI.DbleFibers || S.GUI.RedChannel
+            BpodSystem.Data.Nidaq2Data{currentTrial}=Photo2Data;
+        end
+    end
+    if S.GUI.Wheel
+        BpodSystem.Data.NidaqWheelData{currentTrial}=WheelData;
+    end
+end
+
+%% Save
+if ~isempty(fieldnames(RawEvents))                                          % If trial data was returned
+    BpodSystem.Data = AddTrialEvents(BpodSystem.Data,RawEvents);            % Computes trial events from raw data
+    BpodSystem.Data.TrialSettings(currentTrial) = S;                        % Adds the settings used for the current trial to the Data struct (to be saved after the trial ends)
+    BpodSystem.Data.TrialTypes(currentTrial) = TrialSequence(currentTrial); % Adds the trial type of the current trial to data
+    SaveBpodSessionData;                                                    % Saves the field BpodSystem.Data to the current data file
+end
+
+%% PLOT - extract events from BpodSystem.data and update figures
+try
+[currentOutcome, currentLickEvents]=Online_LickEvents(S.Names.StateToZero{S.GUI.StateToZero});
+FigLick=Online_LickPlot('update',[],FigLick,currentOutcome,currentLickEvents);
+[FigPhoto1,FigPhoto2,FigWheel]=Online_NidaqPlots('update',FigPhoto1,FigPhoto2,FigWheel,currentLickEvents);
+catch
+    disp('Oups, something went wrong with the online analysis... May be you closed a plot ?') 
+end
+
+%% Photometry QC
+if currentTrial==1 && S.GUI.Photometry
+    thismax=max(PhotoData(S.GUI.NidaqSamplingRate:S.GUI.NidaqSamplingRate*2,1))
+    if thismax>4 || thismax<0.3
+        disp('WARNING - Something is wrong with fiber #1 - run check-up! - unpause to ignore')
+        BpodSystem.Pause=1;
+        HandlePauseCondition;
+    end
+    if S.GUI.DbleFibers
+    thismax=max(Photo2Data(S.GUI.NidaqSamplingRate:S.GUI.NidaqSamplingRate*2,1))
+    if thismax>4 || thismax<0.3
+        disp('WARNING - Something is wrong with fiber #2 - run check-up! - unpause to ignore')
+        BpodSystem.Pause=1;
+        HandlePauseCondition;
+    end
+    end
+end
+
+%% End of trial
+HandlePauseCondition; % Checks to see if the protocol is paused. If so, waits until user resumes.
+if BpodSystem.BeingUsed == 0
+    return
+end
+end
+try
+    sprintf('Water collected : %.0d ul', FigLick.water)
+    Analysis_Photometry_Launcher;
+catch
+    disp('Post recording analysis failed - check whether analysis pipeline is present')
+end
+end
---
+++ OptoLickToReward/OptoLickToReward_TaskParameters.m
@@ -0,0 +1,126 @@
+function OptoLickToReward_TaskParameters(Param)
+%
+%
+
+global S
+    S.Names.StateToZero={'CueDelivery','PostOutcome'};
+    S.Names.StateToStim={'na'};
+    S.Names.PPProtocols={'Train_10Hz_500ms_5ms_5V','Pulse_500ms_5V','Pulse_500ms_5V_ch2'};
+    S.Names.Rig=Param.rig;
+
+%% General Parameters    
+    S.GUI.Bonsai=0;
+    S.GUIMeta.Bonsai.Style='checkbox';
+    S.GUIMeta.Bonsai.String='Auto';
+    S.GUI.Wheel=0;
+    S.GUIMeta.Wheel.Style='checkbox';
+    S.GUIMeta.Wheel.String='Auto';
+    S.GUI.Optogenetic=1;
+    S.GUIMeta.Optogenetic.Style='checkbox';
+    S.GUIMeta.Optogenetic.String='Auto';
+ 	S.GUI.Photometry=0;
+    S.GUIMeta.Photometry.Style='checkbox';
+    S.GUIMeta.Photometry.String='Auto';
+    S.GUI.DbleFibers=0;
+    S.GUIMeta.DbleFibers.Style='checkbox';
+    S.GUIMeta.DbleFibers.String='Auto';
+    S.GUI.Isobestic405=0;
+    S.GUIMeta.Isobestic405.Style='checkbox';
+    S.GUIMeta.Isobestic405.String='Auto';
+    S.GUI.RedChannel=0;
+    S.GUIMeta.RedChannel.Style='checkbox';
+    S.GUIMeta.RedChannel.String='Auto';    
+    S.GUIPanels.Recording={'Bonsai','Wheel','Photometry','DbleFibers','Isobestic405','RedChannel','Optogenetic'};
+         
+    S.GUI.Opto_PowerMax=5;
+    S.GUI.Opto_PowerMin=0;
+    S.GUI.Opto_PowerNb=6;
+
+    S.GUIPanels.OptoTuning={'Opto_PowerMax','Opto_PowerMin','Opto_PowerNb'};
+    S.GUITabs.General={'Recording','OptoTuning'};
+
+%% Figure
+    S.GUI.StateToZero=2;
+	S.GUIMeta.StateToZero.Style='popupmenu';
+    S.GUIMeta.StateToZero.String=S.Names.StateToZero;
+    S.GUI.TimeMin=-4;
+    S.GUI.TimeMax=4;
+    S.GUI.BaselineBegin=1.5;
+    S.GUI.BaselineEnd=2.5;
+    S.GUI.NidaqMin=-5;
+    S.GUI.NidaqMax=10;
+
+    S.GUIPanels.PlotPhotometry={'StateToZero','TimeMin','TimeMax','BaselineBegin','BaselineEnd','NidaqMin','NidaqMax'};
+    S.GUITabs.Figure={'PlotPhotometry'};
+
+%% Timing
+    S.GUI.MaxTrials=100;
+    S.GUI.PreCue=3;
+    S.GUI.CueDuration=0.5;
+    S.GUI.Delay=0;
+    S.GUI.DelayIncrement=0;
+    S.GUI.PostOutcome=5;
+    S.GUI.ITI=5;
+    S.GUIPanels.Timing={'MaxTrials','PreCue','CueDuration','Delay','DelayIncrement','PostOutcome','ITI'};
+    S.GUITabs.TaskTiming={'Timing'};
+
+%% Outcome
+    S.GUI.RewardValve=1;
+    S.GUIMeta.RewardValve.Style='popupmenu';
+    S.GUIMeta.RewardValve.String={1,2,3,4,5,6};
+    S.GUI.SmallReward=2;
+    S.GUI.InterReward=5;
+    S.GUI.LargeReward=8;
+    S.GUI.PunishValve=2;
+	S.GUIMeta.PunishValve.Style='popupmenu';
+    S.GUIMeta.PunishValve.String={1,2,3,4,5,6};
+    S.GUI.PunishTime=0.2;
+    S.GUI.OmissionValve=4;
+	S.GUIMeta.OmissionValve.Style='popupmenu';
+    S.GUIMeta.OmissionValve.String={1,2,3,4,5,6};
+    S.GUIPanels.Outcome={'RewardValve','SmallReward','InterReward','LargeReward','PunishValve','PunishTime','OmissionValve'};
+    S.GUITabs.Outcome={'Outcome'};
+    
+%% Nidaq and Photometry
+    S.GUI.PhotometryVersion=1.2;
+    S.GUI.Modulation=1;
+    S.GUIMeta.Modulation.Style='checkbox';
+    S.GUIMeta.Modulation.String='Auto';
+	S.GUI.NidaqDuration=30;
+    S.GUI.NidaqSamplingRate=6100;
+    S.GUI.DecimateFactor=610;
+    S.GUI.modPhase=-1.5708;
+    S.GUI.LED1_Name='Fiber1 470-A1';
+    S.GUI.LED1_Amp=Param.LED1Amp;
+    S.GUI.LED1_Freq=211;
+    S.GUI.LED2_Name='Fiber1 405 / 565';
+    S.GUI.LED2_Amp=Param.LED2Amp;
+    S.GUI.LED2_Freq=531;
+    S.GUI.LED1b_Name='Fiber2 470-mPFC';
+    S.GUI.LED1b_Amp=Param.LED1bAmp;
+    S.GUI.LED1b_Freq=531;
+
+    S.GUIPanels.Photometry={'PhotometryVersion','Modulation','NidaqDuration',...
+                            'NidaqSamplingRate','DecimateFactor','modPhase',...
+                            'LED1_Name','LED1_Amp','LED1_Freq',...
+                            'LED2_Name','LED2_Amp','LED2_Freq',...
+                            'LED1b_Name','LED1b_Amp','LED1b_Freq'};
+                        
+    S.GUITabs.Photometry={'Photometry'};
+%% Optogenetic stimulation
+    S.GUI.PulsePalProtocol=1;
+    S.GUIMeta.PulsePalProtocol.Style='popupmenu';
+    S.GUIMeta.PulsePalProtocol.String=S.Names.PPProtocols;
+    S.GUI.Opto_BNC=Param.BPPP_BNC;
+    S.GUI.Opto_State=1;
+    S.GUIMeta.Opto_State.Style='popupmenu';
+    S.GUIMeta.Opto_State.String=S.Names.StateToStim;
+    S.GUI.Opto_Proba=1;
+    S.GUI.Opto_TrialType=1;
+    S.GUI.Opto_Block=0;
+    S.GUI.Opto_Pairing=0; % Auto-adjust GUI parameters for pairing protocol, used for AuditoryTuning and VisualTuning
+    S.GUIMeta.Opto_Pairing.Style='checkbox';
+    S.GUIMeta.Opto_Pairing.String='Auto'; 
+    S.GUIPanels.Optogenetic={'PulsePalProtocol','Opto_BNC','Opto_State','Opto_Proba','Opto_TrialType','Opto_Block','Opto_Pairing'};
+    S.GUITabs.Opto={'Optogenetic'};              
+end
